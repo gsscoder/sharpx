ba524386577309c078e9e92954c903e3c29f2812
Giacomo Stelluti Scala <gsscoder@gmail.com>
2021-11-27 14:21:03 +0100

    Chained methods in ExceptionExtensions::Format

--- src/SharpX/Extensions/ExceptionExtensions.cs
+++ src/SharpX/Extensions/ExceptionExtensions.cs
@@ -9,18 +9,18 @@ namespace SharpX.Extensions
         {
             Contract.DisallowNull(nameof(exception), exception);
 
-            var builder = new StringBuilder(capacity: 256);
-            builder.AppendLine(exception.Message);
+            var builder = new StringBuilder(capacity: 256)
+                .AppendLine(exception.Message);
             if (exception.StackTrace != null) {
-                builder.AppendLine("--- Stack trace:");
-                builder.AppendLine(exception.StackTrace);
+                builder.AppendLine("--- Stack trace:")
+                       .AppendLine(exception.StackTrace);
             }
             if (exception.InnerException != null) {
-                builder.AppendLine("--- Inner exception:");
-                builder.AppendLine(exception.InnerException.Message);
+                builder.AppendLine("--- Inner exception:")
+                       .AppendLine(exception.InnerException.Message);
                 if(exception.InnerException.StackTrace != null) {
-                    builder.AppendLine("--- Inner exception stack trace:");
-                    builder.AppendLine(exception.InnerException.StackTrace);
+                    builder.AppendLine("--- Inner exception stack trace:")
+                           .AppendLine(exception.InnerException.StackTrace);
                 }
             }
             return builder.ToString();
