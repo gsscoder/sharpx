7565d4b71312e889bc41cd117ccc4cc95f9ec02d
Giacomo Stelluti Scala <gsscoder@gmail.com>
2022-01-10 14:31:14 +0700

    Added Primitives::ToEnumerable methods

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,8 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+- Added `Primitives::ToEnumerable` method (and relative extension method).
+
 ## [1.1.2] - 2021-12-19
 
 - Updated `Strings::ContainsSpecialChar`.
---
+++ tests/SharpX.Specs/Outcomes/PrimitivesSpecs.cs
@@ -0,0 +1,20 @@
+using FluentAssertions;
+using FsCheck;
+using FsCheck.Xunit;
+using SharpX;
+
+namespace Outcomes
+{
+    public class PrimitivesSpecs
+    {
+        [Property(Arbitrary = new[] { typeof(ArbitraryIntegers) })]
+        public void Should_convert_a_value_to_enumerable(int value)
+        {
+            var outcome = Primitives.ToEnumerable(value);
+
+            outcome.Should().NotBeEmpty()
+                .And.HaveCount(1)
+                .And.Contain(value);
+        }
+    }
+}
