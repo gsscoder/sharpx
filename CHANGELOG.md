f2a18894d8a9ff1a3d5934109f9e4e0b808821a7
Giacomo Stelluti Scala <gsscoder@gmail.com>
2022-01-10 14:36:20 +0700

    Added Primitives::FormatException

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -8,6 +8,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ## [Unreleased]
 
 - Added `Primitives::ToEnumerable` method (and relative extension method).
+- Added non-extension version of `ExceptionExtensions::Format` to `Primitives` class.
 
 ## [1.1.2] - 2021-12-19
 
--- src/SharpX/Extensions/ExceptionExtensions.cs
+++ src/SharpX/Extensions/ExceptionExtensions.cs
@@ -5,25 +5,6 @@ namespace SharpX.Extensions
 {
     public static class ExceptionExtensions
     {
-        public static string Format(this Exception exception)
-        {
-            Guard.DisallowNull(nameof(exception), exception);
-
-            var builder = new StringBuilder(capacity: 256)
-                .AppendLine(exception.Message);
-            if (exception.StackTrace != null) {
-                builder.AppendLine("--- Stack trace:")
-                       .AppendLine(exception.StackTrace);
-            }
-            if (exception.InnerException != null) {
-                builder.AppendLine("--- Inner exception:")
-                       .AppendLine(exception.InnerException.Message);
-                if(exception.InnerException.StackTrace != null) {
-                    builder.AppendLine("--- Inner exception stack trace:")
-                           .AppendLine(exception.InnerException.StackTrace);
-                }
-            }
-            return builder.ToString();
-        }
+        public static string Format(this Exception exception) => Primitives.FormatException(exception);
     }
 }
