b3c820a1f4c24e482dace1f17188458b7e8b5b80
Giacomo Stelluti Scala <gsscoder@gmail.com>
2022-04-18 08:48:54 +0200

    Added pragma and used file scoped namespaces (tests project)

--- tests/SharpX.Specs/Outcomes/StringsSpecs.cs
+++ tests/SharpX.Specs/Outcomes/StringsSpecs.cs
@@ -1,3 +1,5 @@
+#pragma warning disable 0618
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,195 +9,194 @@ using FsCheck.Xunit;
 using SharpX;
 using Xunit;
 
-namespace Outcomes
+namespace Outcomes;
+
+public class StringsSpecs
 {
-    public class StringsSpecs
+    static readonly Random _random = new CryptoRandom();
+
+    [Theory]
+    [InlineData('f', 0, "", "")]
+    [InlineData('f', 1, "", "f")]
+    [InlineData('f', 5, " ", "f f f f f")]
+    public void Should_replicate(char value, int count, string separator, string expected)
     {
-        static readonly Random _random = new CryptoRandom();
+        var outcome = Strings.ReplicateChar(value, count, separator);
 
-        [Theory]
-        [InlineData('f', 0, "", "")]
-        [InlineData('f', 1, "", "f")]
-        [InlineData('f', 5, " ", "f f f f f")]
-        public void Should_replicate(char value, int count, string separator, string expected)
+        outcome.Should().Be(expected);
+    }
+
+    [Property(Arbitrary = new[] { typeof(ArbitraryNegativeIntegers) })]
+    public void Trying_to_generate_a_random_string_with_less_than_one_char_raises_ArgumentException(int value)
+    {
+        Action action = () => Strings.Generate(value);
+
+        action.Should().ThrowExactly<ArgumentException>();
+    }
+
+    [Fact]
+    public void Should_generate_an_empty_string_when_length_is_zero()
+    {
+        var outcome = Strings.Generate(0);
+
+        outcome.Should().NotBeNull().And.BeEmpty();
+    }
+
+    [Property(Arbitrary = new[] { typeof(ArbitraryPositiveIntegers) })]
+    public void Should_generate_a_random_string_of_given_length(int value)
+    {
+        var strings = new List<string>() { Strings.Generate(_random.Next(1, 60)) };
+
+        var outcome = Strings.Generate(value);
+
+        outcome.Should().NotBeNull().And.HaveLength(value);
+        strings.Should().NotContain(outcome);
+
+        strings.Add(outcome);
+    }
+
+    [Fact]
+    public void Trying_to_set_allow_quotes_with_disallowed_special_chars_raises_ArgumentException()
+    {
+        Action action = () => Strings.Generate(1, new GenerateOptions()
         {
-            var outcome = Strings.ReplicateChar(value, count, separator);
+            AllowSpecialChars = false,
+            AllowQuoteChars = true
+        });
 
-            outcome.Should().Be(expected);
-        }
+        action.Should().ThrowExactly<ArgumentException>()
+            .WithMessage("Cannot allow quote chars when special chars are disallowed. (Parameter 'options')");
+    }
 
-        [Property(Arbitrary = new[] { typeof(ArbitraryNegativeIntegers) })]
-        public void Trying_to_generate_a_random_string_with_less_than_one_char_raises_ArgumentException(int value)
-        {
-            Action action = () => Strings.Generate(value);
+    [Property(Arbitrary = new[] { typeof(ArbitraryPositiveIntegers) })]
+    public void Should_generate_a_random_string_of_given_length_with_special_chars(int value)
+    {
+        var outcome = Strings.Generate(value + 10,
+            new GenerateOptions { AllowSpecialChars = true, AllowQuoteChars = true });
 
-            action.Should().ThrowExactly<ArgumentException>();
-        }
+        outcome.Should().NotBeNull().And.HaveLength(value + 10);
+        outcome.Any(Strings.IsSpecialChar).Should().BeTrue();
+    }
 
-        [Fact]
-        public void Should_generate_an_empty_string_when_length_is_zero()
-        {
-            var outcome = Strings.Generate(0);
+    [Property(Arbitrary = new[] { typeof(ArbitraryPositiveIntegers) })]
+    public void Should_generate_a_random_string_of_given_length_with_special_chars_no_quotes(int value)
+    {
+        var outcome = Strings.Generate(value + 10,
+            new GenerateOptions { AllowSpecialChars = true, AllowQuoteChars = false });
 
-            outcome.Should().NotBeNull().And.BeEmpty();
-        }
+        outcome.Should().NotBeNull().And.HaveLength(value + 10);
+        outcome.Any(c => c == '"' || c == '\'' || c == '`').Should().BeFalse();
+    }
 
-        [Property(Arbitrary = new[] { typeof(ArbitraryPositiveIntegers) })]
-        public void Should_generate_a_random_string_of_given_length(int value)
-        {
-            var strings = new List<string>() { Strings.Generate(_random.Next(1, 60)) };
+    [Property(Arbitrary = new[] { typeof(ArbitraryPositiveIntegers) })]
+    public void Should_generate_a_random_string_of_given_length_with_prefix(int value)
+    {
+        const string prefix = "prfx_";
 
-            var outcome = Strings.Generate(value);
+        var outcome = Strings.Generate(value, null, prefix);
 
-            outcome.Should().NotBeNull().And.HaveLength(value);
-            strings.Should().NotContain(outcome);
+        outcome.Should().NotBeNull().And.HaveLength(value + prefix.Length);
+        outcome.Should().StartWith(prefix);
+    }
 
-            strings.Add(outcome);
-        }
+    [Theory]
+    [InlineData("")]
+    [InlineData(" ")]
+    [InlineData("  ")]
+    [InlineData(" \n ")]
+    [InlineData(" \r ")]
+    [InlineData(" \t ")]
+    public void Should_not_detect_empty_string_or_whitespace_as_special_character(string value)
+    {
+        var outcome = Strings.ContainsSpecialChar(value);
 
-        [Fact]
-        public void Trying_to_set_allow_quotes_with_disallowed_special_chars_raises_ArgumentException()
-        {
-            Action action = () => Strings.Generate(1, new GenerateOptions()
-            {
-                AllowSpecialChars = false,
-                AllowQuoteChars = true
-            });
+        outcome.Should().BeFalse();
+    }
 
-            action.Should().ThrowExactly<ArgumentException>()
-                .WithMessage("Cannot allow quote chars when special chars are disallowed. (Parameter 'options')");
-        }
+    [Theory]
+    [InlineData("foobar/baz")]
+    [InlineData("foo#?bar/baz")]
+    [InlineData("fo!ob_ar|baz")]
+    [InlineData("foob?ar^baz")]
+    [InlineData("foo@bar_baz")]
+    [InlineData("foobar[b]]az")]
+    [InlineData("f00barbaz_")]
+    public void Should_detect_strings_with_special_characters(string value)
+    {
+        var outcome = Strings.ContainsSpecialChar(value);
 
-        [Property(Arbitrary = new[] { typeof(ArbitraryPositiveIntegers) })]
-        public void Should_generate_a_random_string_of_given_length_with_special_chars(int value)
-        {
-            var outcome = Strings.Generate(value + 10,
-                new GenerateOptions { AllowSpecialChars = true, AllowQuoteChars = true });
+        outcome.Should().BeTrue();
+    }
 
-            outcome.Should().NotBeNull().And.HaveLength(value + 10);
-            outcome.Any(Strings.IsSpecialChar).Should().BeTrue();
-        }
+    [Theory]
+    [InlineData("foobarbaz")]
+    [InlineData("foo bar baz")]
+    [InlineData("fòòbàrbàz")]
+    [InlineData("f00b4rb4z")]
+    public void Should_detect_strings_without_special_characters(string value)
+    {
+        var outcome = Strings.ContainsSpecialChar(value);
 
-        [Property(Arbitrary = new[] { typeof(ArbitraryPositiveIntegers) })]
-        public void Should_generate_a_random_string_of_given_length_with_special_chars_no_quotes(int value)
-        {
-            var outcome = Strings.Generate(value + 10,
-                new GenerateOptions { AllowSpecialChars = true, AllowQuoteChars = false });
+        outcome.Should().BeFalse();
+    }
 
-            outcome.Should().NotBeNull().And.HaveLength(value + 10);
-            outcome.Any(c => c == '"' || c == '\'' || c == '`').Should().BeFalse();
-        }
+    [Theory]
+    [InlineData("foo_bar_baz")]
+    [InlineData("foo bar baz")]
+    [InlineData("fòòbàrbàz")]
+    [InlineData(" \n\r\t" )]
+    [InlineData("f00-b4r-b4z")]
+    public void Should_detect_strings_without_special_characters_except_excluded(string value)
+    {
+        var outcome = Strings.ContainsSpecialChar(value, '_', '-');
 
-        [Property(Arbitrary = new[] { typeof(ArbitraryPositiveIntegers) })]
-        public void Should_generate_a_random_string_of_given_length_with_prefix(int value)
-        {
-            const string prefix = "prfx_";
+        outcome.Should().BeFalse();
+    }
 
-            var outcome = Strings.Generate(value, null, prefix);
+    [Theory]
+    [InlineData("")]
+    [InlineData(" ")]
+    [InlineData("\t")]
+    [InlineData("\r")]
+    [InlineData("\n")]
+    [InlineData("\t \r \n")]
+    public void Should_detect_empty_or_whitespace_string(string value)
+    {
+        var outcome = Strings.IsEmptyWhitespace(value);
 
-            outcome.Should().NotBeNull().And.HaveLength(value + prefix.Length);
-            outcome.Should().StartWith(prefix);
-        }
+        outcome.Should().BeTrue();
+    }
 
-        [Theory]
-        [InlineData("")]
-        [InlineData(" ")]
-        [InlineData("  ")]
-        [InlineData(" \n ")]
-        [InlineData(" \r ")]
-        [InlineData(" \t ")]
-        public void Should_not_detect_empty_string_or_whitespace_as_special_character(string value)
-        {
-            var outcome = Strings.ContainsSpecialChar(value);
+    [Fact]
+    public void Should_get_an_empty_string_when_start_index_and_length_are_zero()
+    {
+        var outcome = Strings.SafeSubstring(string.Empty, 0, 0);
 
-            outcome.Should().BeFalse();
-        }
+        outcome.Should().Be(string.Empty);
+    }
 
-        [Theory]
-        [InlineData("foobar/baz")]
-        [InlineData("foo#?bar/baz")]
-        [InlineData("fo!ob_ar|baz")]
-        [InlineData("foob?ar^baz")]
-        [InlineData("foo@bar_baz")]
-        [InlineData("foobar[b]]az")]
-        [InlineData("f00barbaz_")]
-        public void Should_detect_strings_with_special_characters(string value)
-        {
-            var outcome = Strings.ContainsSpecialChar(value);
+    [Theory]
+    [InlineData("foo")]
+    [InlineData("foo bar")]
+    [InlineData("foo bar baz")]
+    public void Should_get_same_string_when_start_index_and_length_match_string_length(string value)
+    {
+        var outcome = Strings.SafeSubstring(value, 0, value.Length);
 
-            outcome.Should().BeTrue();
-        }
+        outcome.Should().Be(value);
+    }
 
-        [Theory]
-        [InlineData("foobarbaz")]
-        [InlineData("foo bar baz")]
-        [InlineData("fòòbàrbàz")]
-        [InlineData("f00b4rb4z")]
-        public void Should_detect_strings_without_special_characters(string value)
-        {
-            var outcome = Strings.ContainsSpecialChar(value);
+    [Theory]
+    [InlineData("foo")]
+    [InlineData("baar")]
+    [InlineData("baaaz")]
+    public void Should_get_a_substring_without_exception_even_when_length_exceeds_input_string_length(string value)
+    {
+        var input = Strings.Mangle(Strings.Replicate(value, _random.Next(1, 3), separator: "@"),
+            times: 2, maxLength: 3);
+        
+        var outcome = Strings.SafeSubstring(input, 0, input.Length * _random.Next(2, 3));
 
-            outcome.Should().BeFalse();
-        }
-
-        [Theory]
-        [InlineData("foo_bar_baz")]
-        [InlineData("foo bar baz")]
-        [InlineData("fòòbàrbàz")]
-        [InlineData(" \n\r\t" )]
-        [InlineData("f00-b4r-b4z")]
-        public void Should_detect_strings_without_special_characters_except_excluded(string value)
-        {
-            var outcome = Strings.ContainsSpecialChar(value, '_', '-');
-
-            outcome.Should().BeFalse();
-        }
-
-        [Theory]
-        [InlineData("")]
-        [InlineData(" ")]
-        [InlineData("\t")]
-        [InlineData("\r")]
-        [InlineData("\n")]
-        [InlineData("\t \r \n")]
-        public void Should_detect_empty_or_whitespace_string(string value)
-        {
-            var outcome = Strings.IsEmptyWhitespace(value);
-
-            outcome.Should().BeTrue();
-        }
-
-        [Fact]
-        public void Should_get_an_empty_string_when_start_index_and_length_are_zero()
-        {
-            var outcome = Strings.SafeSubstring(string.Empty, 0, 0);
-
-            outcome.Should().Be(string.Empty);
-        }
-
-        [Theory]
-        [InlineData("foo")]
-        [InlineData("foo bar")]
-        [InlineData("foo bar baz")]
-        public void Should_get_same_string_when_start_index_and_length_match_string_length(string value)
-        {
-            var outcome = Strings.SafeSubstring(value, 0, value.Length);
-
-            outcome.Should().Be(value);
-        }
-
-        [Theory]
-        [InlineData("foo")]
-        [InlineData("baar")]
-        [InlineData("baaaz")]
-        public void Should_get_a_substring_without_exception_even_when_length_exceeds_input_string_length(string value)
-        {
-            var input = Strings.Mangle(Strings.Replicate(value, _random.Next(1, 3), separator: "@"),
-                times: 2, maxLength: 3);
-            
-            var outcome = Strings.SafeSubstring(input, 0, input.Length * _random.Next(2, 3));
-
-            outcome.Should().Be(input);
-        }
+        outcome.Should().Be(input);
     }
 }
--- tests/SharpX.Specs/Tests/SimpleValidation.cs
+++ tests/SharpX.Specs/Tests/SimpleValidation.cs
@@ -4,89 +4,88 @@ using FluentAssertions;
 using SharpX;
 using Xunit;
 
-namespace Tests
+namespace Tests;
+
+public class Request
+{
+    public string Name { get; set; }
+    public string EMail { get; set; }
+}
+
+public class Validation
+{
+    public static Result<Request, string> ValidateInput(Request input)
     {
-    public class Request
+        if (input.Name == "") {
+            return Result<Request, string>.FailWith("Name must not be blank");
+        }
+        if (input.EMail == "") {
+            return Result<Request, string>.FailWith("Email must not be blank");
+        }
+        return Result<Request, string>.Succeed(input);
+
+    }
+}
+
+public class SimpleValidation
+{
+    [Fact]
+    public void CanCreateSuccess()
     {
-        public string Name { get; set; }
-        public string EMail { get; set; }
+        var request = new Request { Name = "Giacomo", EMail = "mail@support.com" };
+        var result = Validation.ValidateInput(request);
+        request.Should().Be(result.SucceededWith());
+    }
+}
+
+public class SimplePatternMatching
+{
+    [Fact]
+    public void CanMatchSuccess()
+    {
+        var request = new Request { Name = "Giacomo", EMail = "mail@support.com" };
+        var result = Validation.ValidateInput(request);
+        result.Match(
+            (x, msgs) => { request.Should().Be(x); },
+            msgs => { throw new Exception("wrong match case"); });
     }
 
-    public class Validation
+    [Fact]
+    public void CanMatchFailure()
     {
-        public static Result<Request, string> ValidateInput(Request input)
-        {
-            if (input.Name == "") {
-                return Result<Request, string>.FailWith("Name must not be blank");
-            }
-            if (input.EMail == "") {
-                return Result<Request, string>.FailWith("Email must not be blank");
-            }
-            return Result<Request, string>.Succeed(input);
-
-        }
+        var request = new Request { Name = "Giacomo", EMail = "" };
+        var result = Validation.ValidateInput(request);
+        result.Match(
+            (x, msgs) => { throw new Exception("wrong match case"); },
+            msgs => { "Email must not be blank".Should().Be(msgs.ElementAt(0)); });
     }
+}
 
-    public class SimpleValidation
+public class SimpleEitherPatternMatching
+{
+    [Fact]
+    public void CanMatchSuccess()
     {
-        [Fact]
-        public void CanCreateSuccess()
-        {
-            var request = new Request { Name = "Giacomo", EMail = "mail@support.com" };
-            var result = Validation.ValidateInput(request);
-            request.Should().Be(result.SucceededWith());
-        }
-    }
-
-    public class SimplePatternMatching
-    {
-        [Fact]
-        public void CanMatchSuccess()
-        {
-            var request = new Request { Name = "Giacomo", EMail = "mail@support.com" };
-            var result = Validation.ValidateInput(request);
-            result.Match(
-                (x, msgs) => { request.Should().Be(x); },
-                msgs => { throw new Exception("wrong match case"); });
-        }
-
-        [Fact]
-        public void CanMatchFailure()
-        {
-            var request = new Request { Name = "Giacomo", EMail = "" };
-            var result = Validation.ValidateInput(request);
-            result.Match(
-                (x, msgs) => { throw new Exception("wrong match case"); },
-                msgs => { "Email must not be blank".Should().Be(msgs.ElementAt(0)); });
-        }
-    }
-
-    public class SimpleEitherPatternMatching
-    {
-        [Fact]
-        public void CanMatchSuccess()
-        {
-            var request = new Request { Name = "Giacomo", EMail = "mail@support.com" };
-            var result =
-                Validation
-                    .ValidateInput(request)
-                    .Either(
-                    (x, msgs) => x,
-                    msgs => { throw new Exception("wrong match case"); });
-            request.Should().Be(result);
-        }
-
-        [Fact]
-        public void CanMatchFailure()
-        {
-            var request = new Request { Name = "Giacomo", EMail = "" };
-            var result =
-                Validation.ValidateInput(request)
+        var request = new Request { Name = "Giacomo", EMail = "mail@support.com" };
+        var result =
+            Validation
+                .ValidateInput(request)
                 .Either(
-                    (x, msgs) => { throw new Exception("wrong match case"); },
-                    msgs => msgs.ElementAt(0));
+                (x, msgs) => x,
+                msgs => { throw new Exception("wrong match case"); });
+        request.Should().Be(result);
+    }
 
-            "Email must not be blank".Should().Be(result);
-        }
+    [Fact]
+    public void CanMatchFailure()
+    {
+        var request = new Request { Name = "Giacomo", EMail = "" };
+        var result =
+            Validation.ValidateInput(request)
+            .Either(
+                (x, msgs) => { throw new Exception("wrong match case"); },
+                msgs => msgs.ElementAt(0));
+
+        "Email must not be blank".Should().Be(result);
     }
 }
