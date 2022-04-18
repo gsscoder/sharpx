decf618ce0e49c2f35ae010f2030981429769a6e
Giacomo Stelluti Scala <gsscoder@gmail.com>
2022-04-18 08:48:54 +0200

    chore: use file-scoped namespaces in tests

--- tests/SharpX.Specs/Outcomes/ResultSpecs.cs
+++ tests/SharpX.Specs/Outcomes/ResultSpecs.cs
@@ -1,26 +1,27 @@
+#pragma warning disable 0618
+
 using System;
 using System.Linq;
 using FluentAssertions;
 using SharpX;
 using Xunit;
 
-namespace Outcomes
-{
-    public class Result
-    {
-        [Fact]
-        public void Should_fail_when_Try_catches_an_exception()
-        {
-            var exn = new Exception("Hello World");
-            var result = Result<object, object>.Try(() => { throw exn; });
-            exn.Should().Be(result.FailedWith().First());
-        }
+namespace Outcomes;
 
-        [Fact]
-        public void Should_succeed_when_Try_completes_without_an_exception()
-        {
-            var result = Result<string, object>.Try(() => "hello world");
-            "hello world".Should().Be(result.SucceededWith());
-        }
+public class Result
+{
+    [Fact]
+    public void Should_fail_when_Try_catches_an_exception()
+    {
+        var exn = new Exception("Hello World");
+        var result = Result<object, object>.Try(() => { throw exn; });
+        exn.Should().Be(result.FailedWith().First());
+    }
+
+    [Fact]
+    public void Should_succeed_when_Try_completes_without_an_exception()
+    {
+        var result = Result<string, object>.Try(() => "hello world");
+        "hello world".Should().Be(result.SucceededWith());
     }
 }
--- tests/SharpX.Specs/Outcomes/TrailSpecs.cs
+++ tests/SharpX.Specs/Outcomes/TrailSpecs.cs
@@ -1,43 +1,44 @@
+#pragma warning disable 0618
+
 using System.Collections.Generic;
 using Xunit;
 using FluentAssertions;
 using SharpX;
 
-namespace Outcomes
+namespace Outcomes;
+
+public class TrailSpecs
 {
-    public class TrailSpecs
+    [Fact]
+    public void Should_collect_successes()
     {
-        [Fact]
-        public void Should_collect_successes()
-        {
-            var ok1 = Result<int, string>.Succeed(1, "ok 1");
-            var ok2 = Result<int, string>.Succeed(2);
-            var ok3 = Result<int, string>.Succeed(2, "ok 3");
-            var sut = Trial.Collect(new Result<int, string>[] { ok1, ok2, ok3 });
+        var ok1 = Result<int, string>.Succeed(1, "ok 1");
+        var ok2 = Result<int, string>.Succeed(2);
+        var ok3 = Result<int, string>.Succeed(2, "ok 3");
+        var sut = Trial.Collect(new Result<int, string>[] { ok1, ok2, ok3 });
 
-            var outcome = sut as Ok<IEnumerable<int>, string>;
+        var outcome = sut as Ok<IEnumerable<int>, string>;
 
-            outcome.Should().NotBeNull();
-            outcome.Success.Should().NotBeNullOrEmpty().And
-                .ContainInOrder(1,2);
-            outcome.Messages.Should().NotBeNullOrEmpty().And
-                .ContainInOrder("ok 1", "ok 3");
-        }
+        outcome.Should().NotBeNull();
+        outcome.Success.Should().NotBeNullOrEmpty().And
+            .ContainInOrder(1,2);
+        outcome.Messages.Should().NotBeNullOrEmpty().And
+            .ContainInOrder("ok 1", "ok 3");
+    }
 
-        [Fact]
-        public void Should_propagate_fails()
-        {
-            var ok1 = Result<int, string>.Succeed(1, "ok 1");
-            var ok2 = Result<int, string>.Succeed(2);
-            var fail1 = Result<int, string>.FailWith("non ok 1");
-            var fail2 = Result<int, string>.FailWith("non ok 2");
-            var sut = Trial.Collect(new Result<int, string>[] { ok1, ok2, fail1, fail2 });
+    [Fact]
+    public void Should_propagate_fails()
+    {
+        var ok1 = Result<int, string>.Succeed(1, "ok 1");
+        var ok2 = Result<int, string>.Succeed(2);
+        var fail1 = Result<int, string>.FailWith("non ok 1");
+        var fail2 = Result<int, string>.FailWith("non ok 2");
+        var sut = Trial.Collect(new Result<int, string>[] { ok1, ok2, fail1, fail2 });
 
-            var outcome = sut as Bad<IEnumerable<int>, string>;
+        var outcome = sut as Bad<IEnumerable<int>, string>;
 
-            outcome.Should().NotBeNull();
-            outcome.Messages.Should().NotBeNullOrEmpty().And
-                .ContainInOrder("ok 1", "non ok 1", "non ok 2");
-        }
+        outcome.Should().NotBeNull();
+        outcome.Messages.Should().NotBeNullOrEmpty().And
+            .ContainInOrder("ok 1", "non ok 1", "non ok 2");
     }
 }
