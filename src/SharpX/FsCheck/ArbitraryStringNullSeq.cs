0425e44d3a5d993e85ce3f04ec8494f6c39ce3b3
Giacomo Stelluti Scala <gsscoder@gmail.com>
2023-08-13 17:34:02 +0200

    Refactored ArbitraryStringNullSeq generator class

--- src/SharpX/FsCheck/ArbitraryStringNullSeq.cs
+++ src/SharpX/FsCheck/ArbitraryStringNullSeq.cs
@@ -1,11 +1,18 @@
 using FsCheck;
+using SharpX.Extensions;
 
 namespace SharpX.FsCheck;
 
 static class ArbitraryStringNullSeq
 {
-    public static Arbitrary<string[]> StringSeqGenerator() => Gen.Shuffle(new [] {
-            string.Empty, "one", "1", "two", "2", "three", "3", null, "four", "4", string.Empty, "five",
-            "5", "six", "6", null, "seven", "7", "eight", "8", "nine", "9", null, "ten", "10", string.Empty})
-            .ToArbitrary();
+    public static Arbitrary<string?[]> StringSeqGenerator()
+    {
+        var seq = Primitives.GenerateSeq(() => Strings.Generate(9), count: 20)
+                  .Concat(Enumerable.Range(0, 9).Select(x => x.ToString()))
+                  .Shuffle()
+                  .Intersperse(null)
+                  .Intersperse(string.Empty);
+
+        return Gen.Shuffle(seq).ToArbitrary();
+    }
 }
