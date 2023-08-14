5c20222964a803f9c44a25ca1470a66fed8b296e
Giacomo Stelluti Scala <gsscoder@gmail.com>
2023-08-14 14:07:36 +0200

    feat: implement ArbitraryStringSeq generator

---
+++ src/SharpX/FsCheck/ArbitraryStringSeq.cs
@@ -0,0 +1,15 @@
+using FsCheck;
+using SharpX.Extensions;
+
+namespace SharpX.FsCheck;
+
+static class ArbitraryStringSeq
+{
+    public static Arbitrary<string?[]> Generator()
+    {
+        var seq = Primitives.GenerateSeq(() => Strings.Generate(9), count: 20)
+                  .Concat(Enumerable.Range(0, 9).Select(x => x.ToString()));
+
+        return Gen.Shuffle(seq).ToArbitrary();
+    }
+}
