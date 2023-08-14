f59f8d5e37412f7499e0ca3e11656cfa161edfed
Giacomo Stelluti Scala <gsscoder@gmail.com>
2023-08-14 16:44:08 +0200

    Refactored ArbitraryIntegerSeq::Generator to return a wrapped int[]

--- src/SharpX/FsCheck/ArbitraryIntegerSeq.cs
+++ src/SharpX/FsCheck/ArbitraryIntegerSeq.cs
@@ -5,6 +5,6 @@ namespace SharpX.FsCheck;
 
 static class ArbitraryIntegerSeq
 {
-    public static Arbitrary<FSharpList<int>> Generator() => Gen.ListOf(30,
+    public static Arbitrary<int[]> Generator() => Gen.ArrayOf(30,
         Gen.Choose(-30, 30)).ToArbitrary();
 }
