8d70659e93a6ba3f3e023f4f8f1c16890bbccbfe
Giacomo Stelluti Scala <gsscoder@gmail.com>
2023-08-14 14:00:07 +0200

    chore: standardize Generators method

--- src/SharpX/FsCheck/ArbitraryStringNull.cs
+++ src/SharpX/FsCheck/ArbitraryStringNull.cs
@@ -5,7 +5,7 @@ namespace SharpX.FsCheck;
 
 static class ArbitraryStringNull
 {
-    public static Arbitrary<string> StringGenerator() => Gen.OneOf(
+    public static Arbitrary<string> Generator() => Gen.OneOf(
         Gen.Constant<string>(null),
         Gen.Constant(Strings.Generate(9))).ToArbitrary();
 }
