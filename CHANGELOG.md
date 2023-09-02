bbfe7ca600f94f74c8a88138375bd153667adfda
Giacomo Stelluti Scala <gsscoder@gmail.com>
2023-09-02 13:17:41 +0200

    Implemented IsNumber method and extension method

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -8,6 +8,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ## [Unreleased]
 
 - Implemented `NormalizeToEmpty` method and extension method.
+- Implemented `IsNumber` method and extension method.
 
 ## [6.3.0] - 2023-08-20
 
--- src/SharpX/Extensions/ObjectExtensions.cs
+++ src/SharpX/Extensions/ObjectExtensions.cs
@@ -4,4 +4,7 @@ public static class ObjectExtensions
 {
     /// <summary>Discards a value and return <c>Unit</c>.</summary>
     public static Unit ToUnit<T>(this T value) => Unit.Default;
+
+    /// <summary>Returns true in case of a numeric type value, otherwise false.</summary>
+    public static bool IsNumber<T>(T? value) => Primitives.IsNumber(value);
 }
