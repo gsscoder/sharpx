2054b7183f625b58d6db82c48f1995924699a6e5
Giacomo Stelluti Scala <gsscoder@gmail.com>
2023-09-28 13:15:04 +0700

    Fix: Arbitrary* types made public

--- src/SharpX/FsCheck/ArbitraryString.cs
+++ src/SharpX/FsCheck/ArbitraryString.cs
@@ -3,7 +3,7 @@ using FsCheck.Fluent;
 
 namespace SharpX.FsCheck;
 
-static class ArbitraryString
+public static class ArbitraryString
 {
     public static Arbitrary<string> Generator() => Gen.OneOf(Gen.Constant<string>(null),
         Gen.Constant(Strings.Generate(9))).ToArbitrary();
