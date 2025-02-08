2f0f377e353b21c6933b277bcf45a05979ca48ca
Giacomo Stelluti Scala <gsscoder@gmail.com>
2025-02-08 21:02:57 +0100

    Improved LoggerExtensions methods

--- src/SharpX/Extensions/LoggerExtensions.cs
+++ src/SharpX/Extensions/LoggerExtensions.cs
@@ -4,19 +4,25 @@ namespace SharpX.Extensions;
 
 public static class LoggerExtensions
 {
-    public static T? FailWith<T>(this ILogger logger, string message, T? returnValue = default)
+    public static T? FailWith<T>(
+        this ILogger logger, string message, T? returnValue = default, params object[] args)
     {
         Guard.DisallowNull(nameof(logger), logger);
+        Guard.DisallowNull(nameof(message), message);
 
-        logger.LogError(message);
+        logger.LogError(message, args);
+
         return returnValue;
     }
 
-    public static T? PanicWith<T>(this ILogger logger, string message, Exception? ex = null, T? returnValue = default)
+    public static T? PanicWith<T>(
+        this ILogger logger, string message, Exception? ex = null, T? returnValue = default, params object[] args)
     {
         Guard.DisallowNull(nameof(logger), logger);
+        Guard.DisallowNull(nameof(message), message);
 
-        logger.LogCritical(ex, message);
+        logger.LogCritical(ex, message, args);
+
         return returnValue;
     }
 }
