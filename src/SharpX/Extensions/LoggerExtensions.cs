6baa78203a7088749d78e6c7a72ff4fb3d6aa469
Giacomo Stelluti Scala <gsscoder@gmail.com>
2025-02-09 09:19:34 +0100

    Updated LoggerExtensions methods signature and parameters validation

--- src/SharpX/Extensions/LoggerExtensions.cs
+++ src/SharpX/Extensions/LoggerExtensions.cs
@@ -5,10 +5,11 @@ namespace SharpX.Extensions;
 public static class LoggerExtensions
 {
     public static T? WarnWith<T>(
-        this ILogger logger, string message, T? returnValue = default, params object[] args)
+        this ILogger logger, string message, T returnValue, params object[] args)
     {
         Guard.DisallowNull(nameof(logger), logger);
         Guard.DisallowNull(nameof(message), message);
+        if (!typeof(T).IsValueType) Guard.DisallowNull(nameof(returnValue), returnValue);
 
         logger.LogWarning(message, args);
 
@@ -16,10 +17,11 @@ public static class LoggerExtensions
     }
 
     public static T? FailWith<T>(
-        this ILogger logger, string message, T? returnValue = default, params object[] args)
+        this ILogger logger, string message, T returnValue, params object[] args)
     {
         Guard.DisallowNull(nameof(logger), logger);
         Guard.DisallowNull(nameof(message), message);
+        if (!typeof(T).IsValueType) Guard.DisallowNull(nameof(returnValue), returnValue);
 
         logger.LogError(message, args);
 
@@ -27,10 +29,11 @@ public static class LoggerExtensions
     }
 
     public static T? PanicWith<T>(
-        this ILogger logger, string message, Exception? ex = null, T? returnValue = default, params object[] args)
+        this ILogger logger, string message, T returnValue, Exception? ex = null, params object[] args)
     {
         Guard.DisallowNull(nameof(logger), logger);
         Guard.DisallowNull(nameof(message), message);
+        if (!typeof(T).IsValueType) Guard.DisallowNull(nameof(returnValue), returnValue);
 
         logger.LogCritical(ex, message, args);
 
