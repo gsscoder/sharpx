d1b8e46178e080896805835e45aec4d9ee636982
Giacomo Stelluti Scala <gsscoder@gmail.com>
2025-02-09 13:14:03 +0100

    Added LoggerExtensions methods for handling booleans

--- src/SharpX/Extensions/LoggerExtensions.cs
+++ src/SharpX/Extensions/LoggerExtensions.cs
@@ -4,6 +4,10 @@ namespace SharpX.Extensions;
 
 public static class LoggerExtensions
 {
+    public static bool WarnWith(
+        this ILogger logger, string message, bool returnValue = false, params object[] args) =>
+        WarnWith<bool>(logger, message, returnValue, args);
+
     public static T? WarnWith<T>(
         this ILogger logger, string message, T returnValue, params object[] args)
     {
@@ -16,6 +20,10 @@ public static class LoggerExtensions
         return returnValue;
     }
 
+    public static bool FailWith(
+        this ILogger logger, string message, bool returnValue = false, params object[] args) =>
+        FailWith<bool>(logger, message, returnValue, args);
+
     public static T? FailWith<T>(
         this ILogger logger, string message, T returnValue, params object[] args)
     {
@@ -27,6 +35,10 @@ public static class LoggerExtensions
 
         return returnValue;
     }
+
+    public static bool PanicWith(
+        this ILogger logger, string message, bool returnValue = false, Exception? ex = null, params object[] args) =>
+        PanicWith<bool>(logger, message, returnValue, ex, args);
 
     public static T? PanicWith<T>(
         this ILogger logger, string message, T returnValue, Exception? ex = null, params object[] args)
