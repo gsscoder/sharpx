e806cd5eeecea79b3f4a555b8111831c19ae8eeb
Giacomo Stelluti Scala <gsscoder@gmail.com>
2025-02-22 17:09:31 +0100

    chore: reorganize Either type source files

---
+++ src/SharpX/Types/Either/EitherOfT.cs
@@ -0,0 +1,49 @@
+
+#pragma warning disable 8600, 8601, 8603, 8604, 8616, 8618
+namespace SharpX;
+
+/// <summary>The <c>Either</c> type represents values with two possibilities: a value of type
+/// <c>Either</c> T U is either <c>Left</c> T or <c>Right</c> U. The <c>Either</c> type is
+/// sometimes used to represent a value which is either correct or an error; by convention, the
+/// <c>Left</c> constructor is used to hold an error value and the <c>Right</c> constructor is
+/// used to hold a correct value (mnemonic: "right" also means "correct").</summary>
+public struct Either<TLeft, TRight>
+{
+    readonly TLeft _leftValue;
+    readonly TRight _rightValue;
+
+    internal Either(TLeft value)
+    {
+        _leftValue = value;
+        _rightValue = default;
+        Tag = EitherType.Left;
+    }
+
+    internal Either(TRight value)
+    {
+        _leftValue = default;
+        _rightValue = value;
+        Tag = EitherType.Right;
+    }
+
+    public EitherType Tag { get; private set; }
+
+    #region Basic match methods
+    /// <summary>Matches a <c>Left</c> value returning <c>true</c> and value itself via an output
+    /// parameter.</summary>
+    public bool MatchLeft(out TLeft? value)
+    {
+        value = Tag == EitherType.Left ? _leftValue : default;
+        return Tag == EitherType.Left;
+    }
+
+    /// <summary>Matches a <c>Right</c> value returning <c>true</c> and value itself via an output
+    /// parameter.</summary>
+    public bool MatchRight(out TRight? value)
+    {
+        value = Tag == EitherType.Right ? _rightValue : default;
+        return Tag == EitherType.Right;
+    }
+    #endregion
+}
+
---
+++ src/SharpX/Types/Either/EitherType.cs
@@ -0,0 +1,10 @@
+﻿namespace SharpX;
+
+/// <summary>Discriminator for <c>Either</c>.</summary>
+public enum EitherType
+{
+    /// <summary>Failed computation case.</summary>
+    Left,
+    /// <summary>Sccessful computation case.</summary>
+    Right
+}
