20236c12bbcb1696685f8e239f8a561e6cc88879
Giacomo Stelluti Scala <gsscoder@gmail.com>
2025-02-22 15:56:27 +0100

    Reorganized Maybe type source files

---
+++ src/SharpX/Types/Maybe/MaybeType.cs
@@ -0,0 +1,10 @@
+﻿namespace SharpX;
+
+/// <summary>Discriminator for <c>Maybe</c>.</summary>
+public enum MaybeType
+{
+    /// <summary>Computation case without a value.</summary>
+    Nothing,
+    /// <summary>Computation case with a value.</summary>
+    Just
+}
