6b4c07fa16e8676bf42ca8ed1640ffd05a4c3219
Giacomo Stelluti Scala <gsscoder@gmail.com>
2025-02-22 17:13:52 +0100

    Reorganized Result type source files

---
+++ src/SharpX/Types/Result/Ok.cs
@@ -0,0 +1,22 @@
+﻿namespace SharpX;
+
+/// <summary>Represents the result of a successful computation.</summary>
+public sealed class Ok<TSuccess, TMessage> : Result<TSuccess, TMessage>
+{
+    readonly TSuccess _success;
+    readonly IEnumerable<TMessage> _messages;
+
+    public Ok(TSuccess success, IEnumerable<TMessage> messages)
+        : base(ResultType.Ok)
+    {
+        Guard.DisallowNull(nameof(success), success);
+        Guard.DisallowNull(nameof(messages), messages);
+
+        _success = success;
+        _messages = messages;
+    }
+
+    public TSuccess Success => _success;
+
+    public IEnumerable<TMessage> Messages => _messages;
+}
